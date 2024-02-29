9d2e03cb2d244b826db48c08e235ea9ebfa22d63
benhamin36 <bigbentheamazing@gmail.com>
2024-02-28 23:15:15 -0700

    Display username

--- inventory.js
+++ inventory.js
@@ -5,31 +5,25 @@ const ItemTypes = {
 	Spear: Symbol("spear")
 }
 
-//Represents any kind of item. Type is an ItemType, value is its internal name, name is external name, and power is the amount of damage it does
-class Item {
-    constructor(type, value, name, power) {
-        this.type = type;
-        this.value = value;
-        this.name = name;
-        this.power = power;
-    }
-}
-
 function setEquipped() {
     let armor = localStorage.getItem("CurrentArmor");
     let bow = localStorage.getItem("CurrentBow");
     let sword = localStorage.getItem("CurrentSword");
     let spear = localStorage.getItem("CurrentSpear");
+    let username = localStorage.getItem("userName");
 
     const armorSelect = document.querySelector("#armors");
     const bowSelect = document.querySelector("#bows");
     const swordSelect = document.querySelector("#swords");
     const spearSelect = document.querySelector("#spears");
+    const name = document.querySelector("#username");
 
     armorSelect.value = armor;
     bowSelect.value = bow;
     swordSelect.value = sword;
     spearSelect.value = spear;
+
+    name.textContent = username;
 }
 
 function equipArmor() {
