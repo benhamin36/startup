72184b5115545ea15dae3d92016711a5b3b0ead3
benhamin36 <bigbentheamazing@gmail.com>
2024-03-22 23:11:57 -0600

    Reformat friends screen to work with the post

--- public/friends.js
+++ public/friends.js
@@ -1,35 +1,36 @@
 // Function to generate HTML table from titles
+let friends = [];
 async function generateTable() {
     let username = localStorage.getItem("userName");
-    let request = `{\"req\":\"${username}\"}`
-
+    let request = `{"req":"${username}"}`
+    console.log(JSON.stringify(request));
     const response = await fetch('/api/friends', {
         method: 'POST',
-        headers: {'content-type': 'application/json'},
+        headers: {'content-type': 'application.json'},
         body: JSON.stringify(request),
     });
     const friendList = await response.json();
 
-    const friends = json.stringify(friendList);
+    friends = friendList;
 
-    var tableHtml = '<table><tr>';
-    friends.forEach(function(friend) {
-        tableHtml += '<th>' + friend + '</th>';
-    });
-    tableHtml += '</tr><tr>';
-    friends.forEach(function(friend) {
-        tableHtml += '<td><p id="' + friend.replace(/\s+/g, '') + '">' + friend + '</p></td>';
-    });
-    tableHtml += '</tr></table>';
+    var tableHtml = '<table class="table border-top">';
+    for (var i in friends)
+        tableHtml += `<tr scope="row"><td><img class="rounded" src="Spartan Icon.jpg" width="50px" height="50px"></td><td>${friends[i]}</td><td><form method="get" action="battle.html"><button class="btn btn-secondary" type="submit" onclick="battle(${i})">Battle!</button></form></td><td class="text-success">Active</td></tr>`;
+
+    tableHtml += '</table>';
     return tableHtml;
 }
 
-// Get the container element
-var tableContainer = document.getElementById('tableContainer');
+document.addEventListener('DOMContentLoaded', async () => {
 
-// Generate and insert the table HTML into the container
-tableContainer.innerHTML = generateTable(titles);
+    // Get the container element
+    var tableContainer = document.getElementById('tableContainer');
+
+    // Generate and insert the table HTML into the container
+    tableContainer.innerHTML = await generateTable(friends);
+});
+
 
 function battle(opponent) {
-    localStorage.setItem("Opponent", );
+    localStorage.setItem("Opponent", friends[opponent]);
 }
